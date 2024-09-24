221754f99c9e5dc8b332d4e9218c8cd333b7c554
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2024-09-24 15:16:12 +0700

    Add files via upload

---
+++ SPOJ/EIUONCE.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace EIUONCE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            var reader = new Reader(); 
+
+            var quantity = reader.NextInt();
+            SortedSet<int> numbers = new SortedSet<int>();
+
+            for (int t = 0; t < quantity; t++ ) {
+                var numInput = reader.NextInt();
+
+                for (int i = 1 ; i < numInput ; i++)
+                {
+                    var eachNum = reader.NextInt();
+                    numbers.Add(eachNum);
+                }
+
+                foreach (var eachNum in numbers)
+                {
+                    sb.Append(eachNum + " ");
+                }
+
+                
+            }
+            Console.WriteLine(sb);
+
+        }
+    }
+
+
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+
+}
---
+++ SPOJ/EIUPH011.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace EIUPH011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Reader();
+            StringBuilder sb = new StringBuilder();
+
+            var quantity = reader.NextInt(); 
+            HashSet<int> numbers = new HashSet<int>();
+
+            for ( int i = 0; i < quantity; i++ ) { 
+                var eachNumber = reader.NextInt();  
+
+                if ( !numbers.Contains(eachNumber) ) { 
+                    numbers.Add(eachNumber);
+                }
+            }
+
+            foreach ( var Ele in numbers) { 
+                Console.Write(Ele + " ");
+            } 
+            
+        }
+    }
+
+    #region Reader
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+
+    #endregion
+}
