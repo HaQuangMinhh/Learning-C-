f627128921f83bcc813f0600a2bfd5d1f55ef7fe
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2024-09-24 15:02:31 +0700

    Add files via upload

---
+++ SPOJ/EIGIFT1.cs
@@ -0,0 +1,85 @@
+﻿namespace EIUGIFT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Reader();
+            
+            var giftNum = reader.NextInt(); 
+            var WraNum = reader.NextInt(); // paper
+
+            // two Lists
+            var giftSizes = new List<long>(giftNum);
+            var papperSizes = new List<long>(WraNum);
+
+            for (int i = 0; i < giftNum; i++) { 
+                var currentGift = reader.NextLong();
+                giftSizes.Add(currentGift);
+            }
+            
+            for (int i = 0; i < WraNum; i++) { 
+                var currentPapper = reader.NextLong();
+                papperSizes.Add(currentPapper);
+            }
+
+            // Sort - Ascending order
+            //giftSizes.Sort(); 
+            //papperSizes.Sort();
+
+            // cách sort new 
+            giftSizes = giftSizes.OrderBy(i => i).ToList();
+            papperSizes = papperSizes.OrderBy(i => i).ToList();
+
+            var count = 0;
+            for (int i = 0, j = 0; i < papperSizes.Count && j < giftSizes.Count; ) {
+                var sizeGap = (double) papperSizes[i] / giftSizes[j] ;
+
+                if (sizeGap >= 2 && sizeGap <= 3) {
+                    count++;    // count increase 
+                    i++;        // increase both gift and paper
+                    j++; 
+
+                } else if ( sizeGap < 2 ) { // is gift is bigger 
+                    i++;   // increase papper
+
+                } else  { // is gift is smaller  
+                    j++; // increase gift
+                }
+            }
+
+            Console.WriteLine(count); 
+            
+        }
+    }
+
+
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+}
---
+++ SPOJ/EIPAGES.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+
+namespace EIPAGES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Reader();
+            StringBuilder sb = new StringBuilder();
+
+            var numberPages = new List<int>(); // case 
+            var numInput = reader.NextInt();
+            
+            for (int i = 0; i < numInput; i++) { 
+                var eachNum = reader.NextInt();
+                numberPages.Add(eachNum);
+            }
+
+            //sort 
+            numberPages = numberPages.OrderBy(i => i).ToList();
+
+            
+            // core
+            for (int i = 0; i < numberPages.Count ; i++ ) { 
+                var start = numberPages[i] ;
+                sb.Append(start);
+
+                // loop to find chuoi lien tiep 
+                var end = start;
+                var count = 0;
+                
+                // important core 
+                while ( i + 1 < numberPages.Count && numberPages[i + 1] - numberPages[i] == 1 ) {
+                    end = numberPages[i + 1];
+                    count++;
+                    i++;
+                }
+                
+                if (count >= 2) {
+                    sb.Append("-").Append(end);
+                } else if ( count == 1 ) {
+                    sb.Append(" ").Append(end);
+                }
+                sb.Append(" ");
+            }
+
+            Console.WriteLine(sb);
+        }
+
+    }
+
+
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+}
