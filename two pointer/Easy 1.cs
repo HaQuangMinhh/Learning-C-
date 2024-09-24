173221d27ab7bce04fe10f3ac17fcc55bb843a07
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2024-09-24 15:10:32 +0700

    Add files via upload

---
+++ two pointer/Easy 1.cs
@@ -0,0 +1,45 @@
+﻿namespace Easy_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Remove Duplicates from sorted List
+
+            var inputNum = int.Parse(Console.ReadLine());
+            List<int> eachNum = new List<int>();
+            
+
+            // user input
+            for (int i = 0; i < inputNum ; i++) {
+                var currentValue = int.Parse(Console.ReadLine());   
+                eachNum.Add(currentValue);  
+            }
+            
+            int newLength = removeDuplicates(eachNum);
+
+            Console.WriteLine("New Length is: " + newLength);
+            for (int i = 0; i < newLength; i++) {
+                Console.Write(eachNum[i] + " ");
+            }
+
+     
+        }
+
+        public static int removeDuplicates(List<int> eachNum)
+        {
+            if (eachNum.Count == 0) return 0;
+            int i = 0;
+
+            for (int j = 1; j < eachNum.Count; j++)
+            {
+                if (eachNum[j] != eachNum[i])
+                {
+                    i++;
+                    eachNum[i] = eachNum[j];
+                }
+            }
+            return i + 1;
+        }
+    }
+}
---
+++ two pointer/Easy 2.cs
@@ -0,0 +1,46 @@
+﻿namespace Easy_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numInput = int.Parse(Console.ReadLine());
+            var eachNum = new List<int>();
+
+            for (int i = 0; i < numInput; i++) { 
+                var currentValue = int.Parse(Console.ReadLine());   
+                eachNum.Add(currentValue);
+            }
+
+            var result = sortedSquares (eachNum);
+            Console.WriteLine("Sorted Squares: ");
+            for (int i = 0; i < result.Count ; i++) {
+                Console.Write(result[i] + " ");
+            }
+        }
+
+        public static List<int> sortedSquares (List<int> eachNum) 
+        {
+            int eachValue = eachNum.Count;  // lấy số lượng phần tử 
+            
+            var sortedResult = new List<int>(new int[eachValue]); 
+            
+            int left = 0 , right = eachValue - 1;
+            var index = eachValue - 1; // variable follow position
+
+            while (left <= right) {
+                if (Math.Abs(eachNum[left]) > Math.Abs(eachNum[right]))
+                {
+                    sortedResult[index--] = eachNum[left] * eachNum[left];
+                    left++ ;
+                }
+                else
+                {
+                     sortedResult[index--] = eachNum[right] * eachNum[right];
+                    right-- ;
+                }
+            }
+            return sortedResult;
+        }
+    }
+}
