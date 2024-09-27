4e850748f6c7a28408f2f5e7b9654e66a97460c5
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2024-09-27 09:02:05 +0700

    Add files via upload

---
+++ SPOJ/EIKMAX.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace EIKMAX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Reader(); 
+            StringBuilder sb = new StringBuilder();
+
+            var numInput = reader.NextInt();  
+            var k = reader.NextInt();
+
+            var numbers = new List<int>();
+
+            for ( int i = 0; i < numInput; i++ ) { 
+                var eachNum = reader.NextInt();
+                numbers.Add( eachNum );
+            }
+
+            numbers = numbers.OrderByDescending( x => x ).Take(k).ToList();
+            
+            foreach (var show in numbers) { 
+                Console.Write( show + " " );
+            }
+
+        }
+    }
+
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+}
