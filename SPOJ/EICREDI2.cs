7bf514af614d1618bc149b2eba4eeb67f9789dc0
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2024-10-06 11:36:29 +0700

    Add files via upload

---
+++ SPOJ/EICREDI2.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+namespace EICREDI2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Reader();
+            StringBuilder sb = new StringBuilder(); 
+
+            var quantity = reader.NextInt();
+            
+            for ( int i = 0; i < quantity; i++ ) {
+                var name = reader.Next();
+                sb.Append(name + " ");
+
+                long count = 0;
+                long avg = 0;
+
+                var subjectQuantity = reader.NextInt();
+                for (int t = 0; t < subjectQuantity; t++ ) { 
+                    long eachScore = reader.NextLong();
+
+                    if (eachScore >= 50 ) { 
+                        avg += eachScore;
+                        count++;
+                        sb.Append(eachScore + " ");
+                    }
+                }
+
+                if (count == 0)
+                {
+                    sb.Append(0 + "\n");
+                }
+                else {
+                    sb.Append( avg / count + "\n" );
+                }
+
+            }
+            Console.WriteLine( sb );
+
+
+        }
+    }
+
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+}
---
+++ SPOJ/EICREDIT.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace EICREDIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Reader(); 
+            StringBuilder sb = new StringBuilder();
+
+            var quantity = reader.NextInt();
+ 
+            long avg = 0; 
+
+
+            for (int i = 0; i < quantity; i++) { 
+                
+                var name = reader.Next();
+                sb.Append( name + " ");
+
+                var subjectQuantity = reader.NextInt();
+                int creditCount = 0;
+
+                for (int t = 0; t < subjectQuantity ; t++ ) { 
+                    long score = reader.NextLong();
+
+                    if (score >= 50) {
+                        creditCount += 4;
+                        avg += score ; 
+                        
+                    }
+                }
+                sb.Append(creditCount + "\n");
+                //if (count == 0)
+                //{
+                //    sb.Append(0 + "\n");
+                //}
+                //else {
+                //    sb.Append( avg / count + "\n" ); 
+                //}
+            }
+            Console.WriteLine( sb );
+        }
+    }
+
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+}
