37594220c4c01a06d1e0a9ab23baa37c0d47da1e
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2024-11-05 21:45:41 +0700

    Add files via upload

---
+++ SPOJ/EIGRADU/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Globalization;
+
+namespace EIGRADU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Reader();
+            
+            var students = new List<Student>();   // Object Student
+
+            var numStudent = reader.NextInt();
+            var numCredit = reader.NextInt();
+
+            for ( int i = 0; i < numStudent; i++  ) { 
+                string id = reader.Next();
+                string name = reader.Next();
+
+                int numCourse = reader.NextInt();
+                List<long> grades = new List<long>();   // Object grades
+                
+                for ( int t = 0; t < numCourse; t++) {
+                    long eachScore = reader.NextLong();
+                    grades.Add( eachScore);
+                }
+
+
+                var student = new Student( id , name , grades ); // create a đối tượng
+                
+                // tín chỉ 
+                if ( student.CreditsEarned >= numCredit ) {
+                    students.Add(student); 
+                }  
+                
+            }
+
+             students = students.OrderByDescending(s => s.AvgGPA).ThenBy(s => s.Id)
+            .ToList();
+
+            foreach (var show in students) { 
+                Console.WriteLine( show );
+            }
+        }
+    }
+
+
+
+
+
+
+    #region
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+    #endregion
+}
---
+++ SPOJ/EIGRADU/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIGRADU
+{
+    public class Student
+    {
+        public String Id { get; set; }
+        public String Name { get; set; }
+        public List<long> Grades { get; set; }
+        public int CreditsEarned { get; set; }
+        public long AvgGPA { get; set; }
+
+
+
+        public Student( string id , string name, List<long> grades ) {
+            Id = id;
+            Name = name;
+            Grades = grades;
+            
+            CalculateCreditsGPA();
+        }
+
+        //Calculate tín chỉ  và tính GPA
+        public void CalculateCreditsGPA() {
+            CreditsEarned = 0; 
+            long total = 0;
+            int count = 0;
+
+            foreach ( var eachGrade in Grades ) {
+                if ( eachGrade >= 50  ) {
+                    CreditsEarned += 4;
+                    total += eachGrade;
+                    count++; 
+                }
+            }
+
+
+            if (count > 0)
+            {
+                AvgGPA = ( long )total / count;
+            }
+            else { 
+                AvgGPA= 0;
+            }
+        }
+
+
+        public override String ToString()
+        {
+            return $"{Id} {Name} {AvgGPA}";
+        }
+    }
+}
