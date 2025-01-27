f62a18b1a22502307da65078c49ac03de91bdfba
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2025-01-27 15:58:13 +0700

    Add files via upload

---
+++ SPOJ/EISTULI/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace EISTULI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rd = new Reader(); 
+
+            var numStu = rd.NextInt();
+            var k = rd.NextInt();
+
+            List<Student> students = new List<Student>();   
+
+            for ( int i = 0; i < numStu; i++ ) { 
+                long id = rd.NextInt() ;    
+                var name = rd.Next() ;
+                var course = rd.NextInt() ;
+
+                List<double> scores = new List<double>();
+
+                for ( int t = 0; t < course; t++ ) { 
+                    scores.Add( rd.NextDouble() );
+                }
+
+                var student = new Student( id , name , scores );
+                student.CalculateGPAAndCredits();
+
+                students.Add( student );
+            }
+
+            students = students.OrderByDescending(s => s.GPA)
+                               .ThenBy(s => students.IndexOf(s) ).ToList();
+
+
+            var topStudents = students.Take(k).ToList();
+
+            foreach ( var student in topStudents ) { 
+                Console.WriteLine( $"{student.Id} {student.Name} {Math.Round(student.GPA)} {student.Credits}" );
+            }
+        }
+    }
+
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+}
---
+++ SPOJ/EISTULI/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EISTULI
+{
+    internal class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<double> Scores { get; set; } = new List<double>();
+
+        public Student(long id, string name, List<double> scores) { 
+            Id = id;
+            Name = name;
+            Scores = scores;    
+        }
+
+        public int Credits { get; set; }
+        public double GPA { get; set; }
+
+
+        public void CalculateGPAAndCredits() {
+
+            double totalScores = 0;
+            var passCourseCount = 0;
+
+            foreach ( var score in Scores ) {
+
+                if ( score >= 50 ) {
+                    totalScores += score; 
+                    passCourseCount++;
+                }
+            }
+
+            GPA = passCourseCount > 0 ? totalScores / passCourseCount : 0;
+            Credits = passCourseCount * 4;
+        }
+
+    }
+}
